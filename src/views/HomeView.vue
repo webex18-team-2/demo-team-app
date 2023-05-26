aea0b727624a9d1f9b3271c7154fe81a81ad62c6
RinzGit <200fe632@st.nufs.ac.jp>
2023-05-26 19:19:15 +0900

    その他のページを作成しました。

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div class="quiz-link">
+    <a href="http://localhost:8080/quiz">Vueクイズ</a>
+  </div>
+  <div class="pallet-link">
+    <a href="http://localhost:8080/palette">Vueパレット</a>
+  </div>
+  <div class="memo-link">
+    <a href="http://localhost:8080/memo">Vueメモ</a>
+  </div>
 </template>
